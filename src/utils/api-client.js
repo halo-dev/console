49db3aae3187dec56b904a0b37304ed445d5f6e0
Ryan Wang <i@ryanc.cc>
2021-11-24 20:09:57 +0800

    refactor: api client

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,7 +1,6 @@
-import { AdminApiClient, HaloRestAPIClient } from '@halo-dev/admin-api'
+import { AdminApiClient, AxiosInstance, HaloRestAPIClient } from '@halo-dev/admin-api'
 import store from '@/store'
-import { isObject } from '@/utils/util'
-import { message, notification } from 'ant-design-vue'
+import { message } from 'ant-design-vue'
 
 const apiUrl = process.env.VUE_APP_API_URL ? process.env.VUE_APP_API_URL : 'http://localhost:8080'
 
@@ -12,18 +11,10 @@ const haloRestApiClient = new HaloRestAPIClient({
 const apiClient = new AdminApiClient(haloRestApiClient)
 
 haloRestApiClient.interceptors.request.use(
-  async config => {
+  config => {
     const token = store.getters.token
     if (token && token.access_token) {
       config.headers['Admin-Authorization'] = token.access_token
-    }
-
-    // TODO ignore login and refresh token requests
-
-    const expireTime = Date.now() + 1000 * token.expired_at
-    if (Date.now() >= expireTime) {
-      await store.dispatch('refreshToken', token.refresh_token)
-      return config
     }
     return config
   },
@@ -32,44 +23,25 @@ haloRestApiClient.interceptors.request.use(
   }
 )
 
-function getFieldValidationError(data) {
-  if (!isObject(data) || !isObject(data.data)) {
-    return null
-  }
-
-  const errorDetail = data.data
-
-  return Object.keys(errorDetail).map(key => errorDetail[key])
-}
-
 haloRestApiClient.interceptors.response.use(
   response => {
     return response
   },
-  error => {
+  async error => {
+    const token = store.getters.token
+    const originalRequest = error.config
     const response = error.response
     const data = response ? response.data : null
 
     if (data) {
       if (data.status === 400) {
-        const errorDetails = getFieldValidationError(data)
-        notification.error({
-          message: data.message,
-          description: h => {
-            const errorNodes = errorDetails.map(errorDetail => {
-              return h('a-alert', {
-                props: {
-                  message: errorDetail,
-                  banner: true,
-                  showIcon: false,
-                  type: 'error'
-                }
-              })
-            })
-            return h('div', errorNodes)
-          },
-          duration: 10
-        })
+        // TODO
+      } else if (data.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true
+
+        await store.dispatch('refreshToken', token.refresh_token)
+
+        return AxiosInstance(originalRequest)
       }
     } else {
       message.error('网络异常')
