503be67e5e41d69ce3387e8e629d0da0bb5729d7
hongjunlong <2211898719@qq.com>
2022-04-11 21:53:58 +0800

    feat: generate gzip products at build time
    Signed-off-by: hongjunlong <2211898719@qq.com>

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const pkg = require('./package.json')
 const { defineConfig } = require('@vue/cli-service')
 const dynamicThemePlugin = require('./src/webpack/dynamicTheme.js')
 
+const CompressionPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+
 module.exports = defineConfig({
   publicPath: process.env.PUBLIC_PATH,
 
@@ -11,6 +14,18 @@ module.exports = defineConfig({
       args[0].version = pkg.version
       return args
     })
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('compressionPlugin').use(
+        new CompressionPlugin({
+          filename: '[path][base].gz',
+          algorithm: 'gzip',
+          test: productionGzipExtensions,
+          threshold: 10240,
+          minRatio: 0.8,
+          deleteOriginalAssets: false
+        })
+      )
+    }
   },
 
   configureWebpack: {
