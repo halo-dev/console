03ae5c1da7ebd173a92d50d345ab9a7ef7933ba0
SladeGranger <43343180+SladeGranger@users.noreply.github.com>
2022-05-29 18:58:33 +0800

    revert

--- src/store/modules/option.js
+++ src/store/modules/option.js
@@ -6,7 +6,6 @@ const keys = [
   'blog_url',
   'developer_mode',
   'attachment_upload_image_preview_enable',
-  'attachment_EXIF_remove_enable',
   'attachment_upload_max_parallel_uploads',
   'attachment_upload_max_files',
   'sheet_prefix',
