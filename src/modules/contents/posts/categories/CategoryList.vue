c00d367eee40395c4b779a3680674b6037754782
Ryan Wang <i@ryanc.cc>
2022-08-16 14:33:28 +0800

    refactor: category
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/modules/contents/posts/categories/CategoryList.vue
+++ src/modules/contents/posts/categories/CategoryList.vue
@@ -7,23 +7,24 @@ import { apiClient } from "@halo-dev/admin-shared";
 import {
   IconAddCircle,
   IconBookRead,
-  IconList,
-  IconSettings,
   useDialog,
   VButton,
   VCard,
   VPageHeader,
-  VSpace,
 } from "@halo-dev/components";
 import CategoryEditingModal from "./components/CategoryEditingModal.vue";
+import CategoryListItem from "./components/CategoryListItem.vue";
 
 // types
 import type { Category } from "@halo-dev/api-client";
+import type { CategoryTree } from "./utils";
+import { buildCategoriesTree, convertCategoryTreeToCategory } from "./utils";
 
 const dialog = useDialog();
 
 const editingModal = ref(false);
 const categories = ref<Category[]>([] as Category[]);
+const categoriesTree = ref<CategoryTree[]>([] as CategoryTree[]);
 const selectedCategory = ref<Category | null>(null);
 
 const handleFetchCategories = async () => {
@@ -36,12 +37,17 @@ const handleFetchCategories = async () => {
         0
       );
     categories.value = data.items;
+    categoriesTree.value = buildCategoriesTree(data.items);
   } catch (e) {
     console.error("Failed to fetch categories", e);
   }
 };
 
-const handleDelete = async (category: Category) => {
+const handleUpdateInBatch = async () => {
+  // TODO
+};
+
+const handleDelete = async (category: CategoryTree) => {
   dialog.warning({
     title: "确定要删除该分类吗？",
     description: "删除此分类之后，对应文章的关联将被解除。该操作不可恢复。",
@@ -60,8 +66,8 @@ const handleDelete = async (category: Category) => {
   });
 };
 
-const handleOpenEditingModal = (category: Category | null) => {
-  selectedCategory.value = category;
+const handleOpenEditingModal = (category: CategoryTree) => {
+  selectedCategory.value = convertCategoryTreeToCategory(category);
   editingModal.value = true;
 };
 
@@ -104,92 +110,12 @@ onMounted(() => {
           </div>
         </div>
       </template>
-      <ul class="box-border h-full w-full divide-y divide-gray-100" role="list">
-        <li v-for="(category, index) in categories" :key="index">
-          <div
-            class="group relative block cursor-pointer px-4 py-3 transition-all hover:bg-gray-50"
-          >
-            <div
-              class="drag-element absolute inset-y-0 left-0 flex hidden w-3.5 cursor-move items-center bg-gray-100 transition-all hover:bg-gray-200 group-hover:flex"
-            >
-              <IconList class="h-3.5 w-3.5" />
-            </div>
-            <div class="relative flex flex-row items-center">
-              <div class="flex-1">
-                <div class="flex flex-col sm:flex-row">
-                  <span
-                    class="mr-0 truncate text-sm font-medium text-gray-900 sm:mr-2"
-                  >
-                    {{ category.spec.displayName }}
-                  </span>
-                  <VSpace class="mt-1 sm:mt-0"></VSpace>
-                </div>
-                <div class="mt-1 flex">
-                  <span class="text-xs text-gray-500">
-                    /categories/{{ category.metadata.name }}
-                  </span>
-                </div>
-              </div>
-              <div class="flex">
-                <div
-                  class="inline-flex flex-col flex-col-reverse items-end gap-4 sm:flex-row sm:items-center sm:gap-6"
-                >
-                  <FloatingTooltip
-                    v-if="category.metadata.deletionTimestamp"
-                    class="mr-4 hidden items-center sm:flex"
-                  >
-                    <div
-                      class="inline-flex h-1.5 w-1.5 rounded-full bg-red-600"
-                    >
-                      <span
-                        class="inline-block h-1.5 w-1.5 animate-ping rounded-full bg-red-600"
-                      ></span>
-                    </div>
-                    <template #popper> 删除中</template>
-                  </FloatingTooltip>
-                  <div
-                    class="cursor-pointer text-sm text-gray-500 hover:text-gray-900"
-                  >
-                    20 篇文章
-                  </div>
-                  <time class="text-sm text-gray-500">
-                    {{ category.metadata.creationTimestamp }}
-                  </time>
-                  <span class="self-center">
-                    <FloatingDropdown>
-                      <IconSettings
-                        class="cursor-pointer transition-all hover:text-blue-600"
-                      />
-                      <template #popper>
-                        <div class="w-48 p-2">
-                          <VSpace class="w-full" direction="column">
-                            <VButton
-                              v-close-popper
-                              block
-                              type="secondary"
-                              @click="handleOpenEditingModal(category)"
-                            >
-                              修改
-                            </VButton>
-                            <VButton
-                              v-close-popper
-                              block
-                              type="danger"
-                              @click="handleDelete(category)"
-                            >
-                              删除
-                            </VButton>
-                          </VSpace>
-                        </div>
-                      </template>
-                    </FloatingDropdown>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul>
+      <CategoryListItem
+        :categories="categoriesTree"
+        @change="handleUpdateInBatch"
+        @delete="handleDelete"
+        @open-editing="handleOpenEditingModal"
+      />
     </VCard>
   </div>
 </template>
