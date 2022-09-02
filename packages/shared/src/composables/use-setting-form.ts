fbd91dda1945364f0905705c9503be3f00e3fff2
Ryan Wang <i@ryanc.cc>
2022-09-02 21:50:17 +0800

    feat: system configmap setting page
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- packages/shared/src/composables/use-setting-form.ts
+++ packages/shared/src/composables/use-setting-form.ts
@@ -4,11 +4,13 @@ import { apiClient } from "../utils/api-client";
 
 // libs
 import cloneDeep from "lodash.clonedeep";
+import merge from "lodash.merge";
 
 // types
 import type { Ref } from "vue";
 import type { FormKitSetting, FormKitSettingSpec } from "../types/formkit";
 import type { ConfigMap } from "@halo-dev/api-client";
+import type { FormKitSchemaNode } from "@formkit/core";
 
 const initialConfigMap: ConfigMap = {
   apiVersion: "v1alpha1",
@@ -28,6 +30,7 @@ export function useSettingForm(
   const configMapFormData = ref<
     Record<string, Record<string, string>> | undefined
   >();
+
   const saving = ref(false);
 
   const handleFetchSettings = async () => {
@@ -39,7 +42,29 @@ export function useSettingForm(
       const response = await apiClient.extension.setting.getv1alpha1Setting(
         settingName.value
       );
+
       settings.value = response.data as FormKitSetting;
+
+      // init configMapFormData
+      if (!configMapFormData.value) {
+        const { spec: schemaGroups } = settings.value;
+        const initialConfigMapFormData: Record<
+          string,
+          Record<string, string>
+        > = {};
+        schemaGroups.forEach((schemaGroup) => {
+          initialConfigMapFormData[schemaGroup.group] = {};
+          const formSchema = schemaGroup.formSchema as FormKitSchemaNode[];
+          formSchema.forEach((schema) => {
+            // @ts-ignore
+            if ("name" in schema && "$formkit" in schema) {
+              initialConfigMapFormData[schemaGroup.group][schema.name] =
+                schema.value || undefined;
+            }
+          });
+        });
+        configMapFormData.value = cloneDeep(initialConfigMapFormData);
+      }
     } catch (e) {
       console.error(e);
     }
@@ -55,27 +80,27 @@ export function useSettingForm(
       const response = await apiClient.extension.configMap.getv1alpha1ConfigMap(
         configMapName.value
       );
+
       configMap.value = response.data;
 
       const { data } = configMap.value;
 
       if (data) {
-        configMapFormData.value = Object.keys(data).reduce((acc, key) => {
-          // @ts-ignore
-          acc[key] = JSON.parse(data[key]);
-          return acc;
-        }, {});
+        // merge objects value
+        const { spec: schemaGroups } = settings.value || {};
+
+        schemaGroups?.forEach((schemaGroup) => {
+          if (!configMapFormData.value) {
+            return;
+          }
+          configMapFormData.value[schemaGroup.group] = merge(
+            configMapFormData.value[schemaGroup.group] || {},
+            JSON.parse(data[schemaGroup.group] || "{}")
+          );
+        });
       }
     } catch (e) {
       console.error(e);
-    } finally {
-      if (!configMapFormData.value) {
-        configMapFormData.value = settings.value?.spec.reduce((acc, item) => {
-          // @ts-ignore
-          acc[item.group] = {};
-          return acc;
-        }, {});
-      }
     }
   };
 
