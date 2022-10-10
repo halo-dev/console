d9abb02e5f78d9062db48948f3d6f8d04476c379
Ryan Wang <i@ryanc.cc>
2022-10-10 12:04:02 +0800

    refactor: logic of user login
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/components/global-search/GlobalSearchModal.vue
+++ src/components/global-search/GlobalSearchModal.vue
@@ -12,7 +12,7 @@ import {
   IconPages,
   IconUserSettings,
 } from "@halo-dev/components";
-import { computed, markRaw, onMounted, ref, watch, type Component } from "vue";
+import { computed, markRaw, ref, watch, type Component } from "vue";
 import Fuse from "fuse.js";
 import { apiClient } from "@/utils/api-client";
 
@@ -336,6 +336,8 @@ watch(
   () => props.visible,
   (visible) => {
     if (visible) {
+      handleBuildSearchIndex();
+
       setTimeout(() => {
         globalSearchInput.value?.focus();
       }, 100);
@@ -348,10 +350,6 @@ watch(
     }
   }
 );
-
-onMounted(() => {
-  handleBuildSearchIndex();
-});
 
 const onVisibleChange = (visible: boolean) => {
   emit("update:visible", visible);
