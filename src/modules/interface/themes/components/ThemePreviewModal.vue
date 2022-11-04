5fb82de8a86387ca34a444bad780eb8172968aae
Ryan Wang <i@ryanc.cc>
2022-11-04 18:38:43 +0800

    feat: add theme settings support

--- src/modules/interface/themes/components/ThemePreviewModal.vue
+++ src/modules/interface/themes/components/ThemePreviewModal.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
 import LazyImage from "@/components/image/LazyImage.vue";
+import { useSettingForm } from "@/composables/use-setting-form";
 import { useThemeStore } from "@/stores/theme";
 import { apiClient } from "@/utils/api-client";
-import type { Theme } from "@halo-dev/api-client";
+import type { FormKitSchemaCondition, FormKitSchemaNode } from "@formkit/core";
+import type { SettingForm, Theme } from "@halo-dev/api-client";
 import {
   VModal,
   IconLink,
   IconPalette,
+  IconSettings,
   VEntity,
   VEntityField,
   VTag,
+  VTabbar,
+  VButton,
 } from "@halo-dev/components";
 import { storeToRefs } from "pinia";
 import { computed, ref, watch } from "vue";
@@ -32,11 +37,16 @@ const emit = defineEmits<{
   (event: "close"): void;
 }>();
 
+interface SettingTab {
+  id: string;
+  label: string;
+}
+
 const { activatedTheme } = storeToRefs(useThemeStore());
 
-const themesVisible = ref(false);
-
+const previewFrame = ref<HTMLIFrameElement | null>(null);
 const themes = ref<Theme[]>([] as Theme[]);
+const themesVisible = ref(false);
 const selectedTheme = ref<Theme>();
 
 const handleFetchThemes = async () => {
@@ -69,6 +79,11 @@ const onVisibleChange = (visible: boolean) => {
   }
 };
 
+const handleOpenThemes = () => {
+  settingsVisible.value = false;
+  themesVisible.value = !themesVisible.value;
+};
+
 const handleSelect = (theme: Theme) => {
   selectedTheme.value = theme;
 };
@@ -81,6 +96,59 @@ const previewUrl = computed(() => {
     selectedTheme.value.metadata.name
   }`;
 });
+
+// theme settings
+const settingTabs = ref<SettingTab[]>([] as SettingTab[]);
+const activeSettingTab = ref("");
+const settingsVisible = ref(false);
+
+const settingName = computed(() => selectedTheme.value?.spec.settingName);
+const configMapName = computed(() => selectedTheme.value?.spec.configMapName);
+
+const {
+  setting,
+  configMapFormData,
+  saving,
+  handleFetchConfigMap,
+  handleFetchSettings,
+  handleSaveConfigMap,
+} = useSettingForm(settingName, configMapName);
+
+watch(
+  () => selectedTheme.value,
+  async () => {
+    if (selectedTheme.value) {
+      await handleFetchSettings();
+      await handleFetchConfigMap();
+
+      if (setting.value) {
+        const { forms } = setting.value.spec;
+        settingTabs.value = forms.map((item: SettingForm) => {
+          return {
+            id: item.group,
+            label: item.label || "",
+          };
+        });
+      }
+
+      activeSettingTab.value = settingTabs.value[0].id;
+    }
+  }
+);
+
+const handleOpenSettings = () => {
+  themesVisible.value = false;
+  settingsVisible.value = !settingsVisible.value;
+};
+
+const formSchema = computed(() => {
+  if (!setting.value) {
+    return;
+  }
+  const { forms } = setting.value.spec;
+  return forms.find((item) => item.group === activeSettingTab.value)
+    ?.formSchema as (FormKitSchemaCondition | FormKitSchemaNode)[];
+});
 </script>
 <template>
   <VModal
@@ -92,8 +160,11 @@ const previewUrl = computed(() => {
     @update:visible="onVisibleChange"
   >
     <template #actions>
-      <span @click="themesVisible = !themesVisible">
+      <span @click="handleOpenThemes">
         <IconPalette />
+      </span>
+      <span @click="handleOpenSettings">
+        <IconSettings />
       </span>
       <span>
         <a :href="previewUrl" target="_blank">
@@ -112,14 +183,59 @@ const previewUrl = computed(() => {
         leave-from-class="translate-x-0"
         leave-to-class="-translate-x-full"
       >
-        <div v-show="themesVisible" class="h-full w-96 overflow-y-auto">
+        <div
+          v-show="themesVisible || settingsVisible"
+          class="relative h-full w-96 overflow-y-auto"
+        >
+          <div v-if="settingsVisible" class="mb-20">
+            <VTabbar
+              v-model:active-id="activeSettingTab"
+              :items="settingTabs"
+              class="w-full overflow-x-auto overflow-y-hidden !rounded-none"
+              type="outline"
+            ></VTabbar>
+            <div class="bg-white p-2">
+              <div v-for="(tab, index) in settingTabs" :key="index">
+                <FormKit
+                  v-if="
+                    tab.id === activeSettingTab &&
+                    configMapFormData &&
+                    formSchema
+                  "
+                  :id="tab.id"
+                  :key="tab.id"
+                  v-model="configMapFormData[tab.id]"
+                  :name="tab.id"
+                  :actions="false"
+                  :preserve="true"
+                  type="form"
+                  @submit="handleSaveConfigMap"
+                >
+                  <FormKitSchema :schema="formSchema" />
+                </FormKit>
+              </div>
+              <div v-permission="['system:configmaps:manage']" class="pt-5">
+                <div class="flex justify-start">
+                  <VButton
+                    :loading="saving"
+                    type="secondary"
+                    @click="$formkit.submit(activeSettingTab || '')"
+                  >
+                    保存
+                  </VButton>
+                </div>
+              </div>
+            </div>
+          </div>
           <ul
+            v-if="themesVisible"
             class="box-border h-full w-full divide-y divide-gray-100"
             role="list"
           >
             <li
               v-for="(theme, index) in themes"
               :key="index"
+              class="group"
               @click="handleSelect(theme)"
             >
               <VEntity
@@ -178,14 +294,32 @@ const previewUrl = computed(() => {
                     </template>
                   </VEntityField>
                 </template>
+
+                <template #end>
+                  <VEntityField v-permission="['system:themes:manage']">
+                    <template #description>
+                      <VButton
+                        class="hidden group-hover:inline-flex"
+                        size="sm"
+                        @click="handleOpenSettings"
+                      >
+                        设置
+                      </VButton>
+                    </template>
+                  </VEntityField>
+                </template>
               </VEntity>
             </li>
           </ul>
+          <div v-if="settingsVisible" class="fixed bottom-2 left-2 bg-white">
+            <VButton size="md" @click="handleOpenThemes">返回</VButton>
+          </div>
         </div>
       </transition>
       <div class="h-full flex-1">
         <iframe
           v-if="visible"
+          ref="previewFrame"
           class="h-full w-full border-none transition-all duration-300"
           :src="previewUrl"
         ></iframe>
