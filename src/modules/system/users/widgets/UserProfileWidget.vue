e96e74cdd87643b019ada9d8357718d6c82c4b06
Ryan Wang <i@ryanc.cc>
2022-11-30 14:13:27 +0800

    feat: refine UserProfileWidget

--- src/modules/system/users/widgets/UserProfileWidget.vue
+++ src/modules/system/users/widgets/UserProfileWidget.vue
@@ -1,4 +1,53 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useUserStore } from "@/stores/user";
+import { VAvatar, VCard } from "@halo-dev/components";
+import { storeToRefs } from "pinia";
+
+const userStore = useUserStore();
+const { currentUser } = storeToRefs(userStore);
+</script>
 <template>
-  <div>asd</div>
+  <VCard class="h-full">
+    <div class="flex flex-col gap-2">
+      <div class="flex flex-row items-center gap-3">
+        <div class="inline-flex">
+          <VAvatar
+            size="lg"
+            :src="currentUser?.spec.avatar"
+            :alt="currentUser?.spec.displayName"
+            circle
+          />
+        </div>
+        <div class="inline-flex flex-col">
+          <div class="text-base font-semibold">
+            {{ currentUser?.spec.displayName }}
+          </div>
+          <div class="text-xs text-gray-500">
+            @{{ currentUser?.metadata.name }}
+          </div>
+        </div>
+      </div>
+      <div class="grid grid-cols-4">
+        <div class="flex flex-col items-center justify-center">
+          <div class="text-sm tabular-nums text-gray-900">12</div>
+          <div class="text-[0.6rem] font-light text-gray-500">文章</div>
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="text-sm tabular-nums text-gray-900">12</div>
+          <div class="text-[0.6rem] font-light text-gray-500">文章</div>
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="text-sm tabular-nums text-gray-900">12</div>
+          <div class="text-[0.6rem] font-light text-gray-500">文章</div>
+        </div>
+
+        <div class="flex flex-col items-center justify-center">
+          <div class="text-sm tabular-nums text-gray-900">12</div>
+          <div class="text-[0.6rem] font-light text-gray-500">文章</div>
+        </div>
+      </div>
+    </div>
+  </VCard>
 </template>
