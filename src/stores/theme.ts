79963275589b016a94009e079014b2990b7bf673
Ryan Wang <i@ryanc.cc>
2023-01-13 11:48:12 +0800

    perf: improve ui permissions for themes

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -2,11 +2,18 @@ import { apiClient } from "@/utils/api-client";
 import type { Theme } from "@halo-dev/api-client";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import { usePermission } from "@/utils/permission";
 
 export const useThemeStore = defineStore("theme", () => {
   const activatedTheme = ref<Theme>();
 
+  const { currentUserHasPermission } = usePermission();
+
   async function fetchActivatedTheme() {
+    if (!currentUserHasPermission(["system:themes:view"])) {
+      return;
+    }
+
     try {
       const { data } = await apiClient.theme.fetchActivatedTheme({
         mute: true,
